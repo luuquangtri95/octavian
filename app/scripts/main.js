1b886a5938dfd3a90d24f5299cb016f21308e9cb
testgit <luuquangtri95@gmail.com>
2021-08-20 15:31:55 +0700

    add slider app

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -22,3 +22,27 @@ $(document).ready(function () {
     ],
   });
 });
+
+$(document).ready(function () {
+  $('.app__screen').slick({
+    dots: false,
+    prevArrow: false,
+    nextArrow: false,
+    centerMode: true,
+    variableWidth: true,
+    slidesToShow: 3,
+    autoplay: true,
+    infinite: true,
+    speed: 2000,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
